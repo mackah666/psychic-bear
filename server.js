2917c36566222ff1a90baca296471422fe768692
unknown <michael.ackah@L17566.isofin.co.uk>
2015-06-12 14:03:01 +0100

    node add cooking

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world mackah666!');
+    res.end('Hello, world mackah666! Cooking on gas!!!!');
     
 }).listen(process.env.PORT || 8080);
