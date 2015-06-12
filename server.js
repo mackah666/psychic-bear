4f5179d69dbce02181885bd35639059639e19ab1
unknown <michael.ackah@L17566.isofin.co.uk>
2015-06-12 11:29:35 +0100

    changed port to 8080

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 var http = require('http');
- 
-var server = http.createServer(function(req, res) {
-    res.writeHead(200);
-    res.write('<p>Hello world mackah!</p>');
-    res.end();
-});
- 
-var port = 80;
-server.listen(port, function() {
-    console.log('server listening on port ' + port);
-});
+
+http.createServer(function (req, res) {
+    
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello, world mackah666!');
+    
+}).listen(process.env.PORT || 8080);
